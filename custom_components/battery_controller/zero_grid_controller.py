f9db6d8a9b730327ab05dfa03c8daa6724e3b811
bvweerd <b.van.weerd@gmail.com>
2026-03-06 08:56:38 +0100

    Revert "bugfix on small refactor"
    This reverts commit 53844d644defa66405d5980dfca9db4000dfcbc6.

--- custom_components/battery_controller/zero_grid_controller.py
+++ custom_components/battery_controller/zero_grid_controller.py
@@ -251,7 +251,6 @@ class ZeroGridController:
         raw_target_w = self.calculate_battery_setpoint(
             current_grid_w,
             current_soc_kwh,
-            current_battery_w,
             dp_schedule_w,
             mode,
         )
